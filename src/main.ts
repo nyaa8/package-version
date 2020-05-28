d8f0d4b2fcda804cb51564f6fda23a8444007427
nyaascii <contrib@nyaa.science>
2020-05-28 12:16:24 +0200

    feat: Main action file and utilities

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { getInput, setFailed, exportVariable } from '@actions/core';
+import { findPackageJson, extract } from './util';
+
+async function run(): Promise<void> {
+    try {
+        const followSymbolicLinks: boolean = getInput('follow-symlinks').toLowerCase() !== 'false';
+        const path: string = getInput('path') || await findPackageJson(followSymbolicLinks);
+
+        const packageVersion: string = await extract(path);
+
+        exportVariable('PACKAGE_VERSION', packageVersion);
+    } catch (error) {
+        setFailed(error.message);
+    }
+}
+
+run();
---
+++ src/util.ts
@@ -0,0 +1,27 @@
+import { create } from '@actions/glob';
+
+interface PackageFile {
+    version: string
+}
+
+const patterns = ['**/package.json', '!**/node_modules'].join('\n');
+
+export async function findPackageJson(followSymlinks: boolean): Promise<string> {
+    const globber = await create(patterns, { followSymbolicLinks: followSymlinks });
+    const files = await globber.glob();
+
+    return files[0];
+}
+
+export async function extract(path: string): Promise<string> {
+    return new Promise(resolve => {
+        const packageFile: PackageFile = require(path);
+
+        try {
+            resolve(packageFile.version);
+        } catch {
+            resolve('0');
+            throw new Error('Invalid package.json format or path');
+        }
+    });
+}
